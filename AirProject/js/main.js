c542d6d7d92d5b681f9f8d816c64ab48a0d6100e
Andrean Januar Priatmojo <an.jp29@gmail.com>
2018-11-15 14:46:24 +0700

    pisah biodata

--- AirProject/js/main.js
+++ AirProject/js/main.js
@@ -187,16 +187,3 @@ jQuery(document).ready(function( $ ) {
   }
 
 });
-
-$(document).ready(function(){
-  $("#owner").hide();
-  $("#worker").hide();
-  $("#btn-owner").on("click", function(){
-    $("#owner").show();
-    $("#worker").hide();
-  });
-  $("#btn-worker").on("click", function(){
-    $("#worker").show();
-    $("#owner").hide();
-  });
-});
